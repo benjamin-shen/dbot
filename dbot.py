0ae6183dcf780be11642cde6162402f2333d9c9d
Benjamin Shen <bfs45@cornell.edu>
2019-04-21 02:32:48 -0400

    added responses

--- dbot.py
+++ dbot.py
@@ -2,6 +2,7 @@
 # handle post requests
 import os
 import time
+import random
 import json
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -26,6 +27,8 @@ understandableDict = {
     
     'fuck': "Watch your fucking language. (Did you mean to say 'duck'?)",
     'bitch': "",
+    'dick': "",
+    'penis': "",
 }
 
 # someone sends a message
@@ -47,6 +50,8 @@ def webhook():
                     if key=='bitch':
                         send_message("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. ")
                         send_message("Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
+                    if key=='dick' or key=='penis':
+                        send_message("%a inches" % (random.randint(1,12)))
                     else:
                         send_message(value);
     
@@ -84,8 +89,10 @@ def create_message(commands):
     else:
         for command in commands:
             if command=="help":
+                result = ""
                 for key,value in responseDict.items():
-                    send_message("dbot " + key + " - " + value)
+                    result += "dbot " + key + " - " + value + "\n"
+                send_message(result)
             elif command=="info":
                 send_message("dbot is a GroupMe bot created by Ben Shen '22. The d stands for Douglas.")
             else:
