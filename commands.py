9366a52b1be20bd842b79f68d9e47fd53bb80884
Benjamin Shen <bfs45@cornell.edu>
2019-04-22 11:53:34 -0400

    moved commands to commands.py

--- commands.py
+++ commands.py
@@ -1,3 +1,9 @@
+import os
+import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+# interpret text files
 responseDict = {}
 with open('dictionaries/response.txt', 'r') as file:
     responseArray = file.readlines()
@@ -7,7 +13,6 @@ with open('dictionaries/response.txt', 'r') as file:
             keyword = line[:colon]
             response = line[colon+1:].strip() # remove white space
             responseDict[keyword] = response
-            
 understandableDict = {}
 with open('dictionaries/understandable.txt', 'r') as file:
     understandableArray = file.readlines()
@@ -18,16 +23,36 @@ with open('dictionaries/understandable.txt', 'r') as file:
             response = line[colon+1:].strip() # remove white space
             understandableDict[keyword] = response
 
+# GroupMe functions
+def send_message(to_send):
+    url  = 'https://api.groupme.com/v3/bots/post'
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text'   : to_send,
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+
+# response
 def help():
     result = ""
     for key,value in responseDict.items():
         result += "dbot " + key + " - " + value + "\n"
+    send_message(result)
     return result
 def info():
-    return "dbot is a GroupMe bot created by Ben Shen '22. The d stands for Douglas."
-    
-    
-# testing
-print(understandableDict)
-print(help())
-print(info())
+    result = "dbot is a GroupMe bot created by Ben Shen '22. The d stands for Douglas."
+    send_message(result)
+    return result
+# understandable
+def inches():
+    length = random.randint(0,12)
+    result = ""
+    if length==0:
+        result = "vagina"
+    elif length==1:
+        result = str(length) + " inch"
+    else:
+        result = str(length) + " inches"
+    send_message(result)
+    return result
--- dbot.py
+++ dbot.py
@@ -1,12 +1,6 @@
 import time
 import random
 
-# handle post requests
-import os
-import json
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -20,48 +14,18 @@ def webhook():
     time.sleep(1)
     name = data['name']
     text = data['text'].lower()
-    
+
     if name != 'dbot':
-        # call bot
-        if text.startswith('dbot'):
-            bot_action(text)
-        # doesn't call bot
-        else:
-            for key,value in dbot.understandableDict.items():
-                if key in text:
-                    if key=='dick' or key=='penis':
-                        length = random.randint(0,12)
-                        if length==0:
-                            send_message("vagina")
-                        elif length==1:
-                            send_message(str(length) + " inch")
-                        else:
-                            send_message(str(length) + " inches")
-                    else:
-                        text = value
-                        while len(text) > 1000: # handle character limit
-                            send_message(text[:1000])
-                            text = text[1000:]
-                            time.sleep(0.1)
-                        send_message(text);
-    
+        if text.startswith('dbot'): # bot is explicitly called
+            bot_commanded(parse(received_text))
+        else: # bot understands something
+            bot_understood()
+        if False: # bot is implicitly called
+            return
     return "ok", 200
 
-# main functions
-def bot_action(received_text):
-    if True: #
-        create_message(parse_message(received_text))
-def send_message(to_send):
-    url  = 'https://api.groupme.com/v3/bots/post'
-    data = {
-        'bot_id' : os.getenv('GROUPME_BOT_ID'),
-        'text'   : to_send,
-    }
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
-
-# helper functions
-def parse_message(msg):
+# functions
+def parse(msg): # breaks down user message
     words = msg.split(" ")
     words.pop(0) # remove call to dbot
     responseArray = []
@@ -72,12 +36,24 @@ def parse_message(msg):
         if word in responseArray:
             commands.append(word)
     return commands
-def create_message(commands):
+def bot_commanded(commands):
     if len(commands)==0:
-        send_message("Try 'dbot help'.")
+        dbot.send_message("Try 'dbot help'.")
     else:
         for command in commands:
             try:
-                send_message(eval("dbot." + command + "()"))
+                eval("dbot." + command + "()")
             except:
-                send_message("Error: command not defined.")
+                dbot.send_message("Error: command not defined.")
+def bot_understood():
+    for key,value in dbot.understandableDict.items():
+        if key in text:
+            if key=='dick' or key=='penis':
+                dbot.inches()
+            else:
+                text = value
+                while len(text) > 1000: # handle character limit
+                    dbot.send_message(text[:1000])
+                    text = text[1000:]
+                    time.sleep(0.1)
+                dbot.send_message(text);
