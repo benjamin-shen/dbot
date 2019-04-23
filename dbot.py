edf4349f03d2faed193ae1624be0b87ef0a3f598
Benjamin Shen <bfs45@cornell.edu>
2019-04-23 01:08:23 -0400

    fixed syntax

--- dbot.py
+++ dbot.py
@@ -35,12 +35,12 @@ def parse(msg): # breaks down user message
             commands.append(word)
         if len(commands)>0:
             # deal with parameters
-            elif word=='-' and i+1<len(words):
+            if word=='-' and i+1<len(words):
                 param = word+words[i+1]
                 if commands[-1]+param in dbot.functions.keys():
                     commands.append(param)
                     words.pop(i+1)
-            if word[:1]=='-' and commands[-1]+word in dbot.functions.keys():
+            elif word[:1]=='-' and commands[-1]+word in dbot.functions.keys():
                 commands.append(word)
         i += 1
     return commands
